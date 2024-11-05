c8150fadff171ddec1e1cd481c6c83df35d06642
c-bejar <p.bejar.christopher@gmail.com>
2024-11-04 19:07:44 -0600

    changes from class

--- sketch.js
+++ sketch.js
@@ -1,4 +1,19 @@
 let buttonPressed = 0;
+let bubbles = []; //Array to store bubble objects
+let houseImage; // Variable to store the house image
+let clamTop, clamBottom; // Variable to store image of clam's top and bottom halves
+let backgroundImage; // Variable to store the background image
+
+let clamWidth = 494 / 2;
+let clamHeight = 270 / 2;
+
+function preload() {
+  // Load the images before the sketch starts
+  houseImage = loadImage("media/house.png");
+  clamTop = loadImage("media/clamTop.png");
+  clamBottom = loadImage("media/clamBottom.png");
+  backgroundImage = loadImage("media/spongebg.png");
+}
 
 function setup() {
   WebMidi.enable(function (err) {
@@ -31,7 +46,15 @@ function setup() {
 
   navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
 
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight); // set as full screen width
+  angleMode(DEGREES);
+
+  // Generate initial bubbles
+  for (let i = 0; i < 20; i++) {
+    bubbles.push(
+      new Bubble(random(width), height + random(100), random(20, 50))
+    );
+  }
 }
 
 function onMIDISuccess(midiAccess) {
@@ -68,6 +91,82 @@ function getMIDIMessage(midiMessage) {
 }
 
 function draw() {
-  background("blue");
+  // background("blue");
   // TODO: Game stuff
+
+  // Draw the background image scaled to window size
+  image(backgroundImage, 0, 0, windowWidth, windowHeight);
+
+  // purely for testing:
+  drawClams();
+
+  // Draw and update each bubble
+  for (let i = 0; i < bubbles.length; i++) {
+    bubbles[i].move();
+    bubbles[i].display();
+
+    // If the bubble goes off the top of the canvas, reset its position
+    if (bubbles[i].y < -bubbles[i].size) {
+      bubbles[i].y = height + random(100);
+      bubbles[i].x = random(width);
+    }
+  }
+  // Draw the house image at the bottom right corner
+  let imgWidth = houseImage.width / 2; // Scale image width
+  let imgHeight = houseImage.height / 2; // Scale image height
+  image(
+    houseImage,
+    width - imgWidth - 5,
+    height - imgHeight - 5,
+    imgWidth,
+    imgHeight
+  ); // Position with some padding
+}
+
+function drawClams() {
+  push();
+  translate(0, windowHeight - clamHeight);
+
+  createClam(0, false);
+  translate(clamWidth + 10, 0);
+  createClam(1, false);
+  translate(clamWidth + 400, 0);
+  createClam(0, true);
+  translate(clamWidth + 10, 0);
+  createClam(0, true);
+  pop();
+}
+
+function createClam(x, flipped) {
+  console.log(x);
+  push();
+  if (flipped) scale(-1, 1);
+  image(clamBottom, 0, 0, clamWidth, clamHeight);
+  push();
+  if (true) {
+    rotate(-15);
+  }
+  image(clamTop, 0, 0, clamWidth, clamHeight);
+  pop();
+  pop();
+}
+
+// Bubble class definition
+class Bubble {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.speed = random(1, 3); // Speed of the bubble
+  }
+
+  move() {
+    this.y -= this.speed; // Move the bubble upward
+  }
+
+  display() {
+    fill(255, 255, 255, 150); // Semi-transparent white color for the bubble
+    noStroke();
+    ellipse(this.x, this.y, this.size, this.size);
+  }
 }
